4dcab198ef0ebe3a1a9445350fb4f48da16487f9
Karan <karansharma192004@gmail.com>
2025-06-15 13:13:52 +0530

    Added styling

--- frontend/src/Pages/Homepage.tsx
+++ frontend/src/Pages/Homepage.tsx
@@ -17,9 +17,9 @@ export default function Homepage() {
     try {
       setLoading(true);
       const res = await axios.post(`${BACKEND_URL}/download`, { url: trimmedUrl });
-      window.location.href = res.data.url;
+      window.open(res.data.url, "_blank");
+      setUrl('')
     } catch (error) {
-      console.error("Download failed:", error);
       alert("Download failed. Please try again.");
     } finally {
       setLoading(false);
@@ -27,7 +27,7 @@ export default function Homepage() {
   };
 
   return (
-    <div className="bg-white w-screen h-screen overflow-hidden flex flex-col items-center justify-center px-5 py-16 md:px-50 md:py-24">
+    <div className="bg-gradient-to-br from-blue-300/90 via-white to-rose-300/90 w-screen h-screen overflow-hidden flex flex-col items-center justify-center px-5 py-16 md:px-50 md:py-24">
       <div className="max-w-3xl text-center space-y-4 mb-10">
         <button className="text-red-600 bg-red-100 rounded-lg p-2 md:px-3 md:py-2 cursor-pointer hover:shadow hover:bg-red-200 transition font-semibold text-xs">
           Welcome to YouTube Downloader
@@ -49,30 +49,12 @@ export default function Homepage() {
         />
         <button
           onClick={startDownload}
-          className="bg-red-600 text-white px-4 py-2 rounded-lg hover:bg-red-700 transition"
+          className="bg-red-600 text-white px-3 text-xs md:text-xl md:px-4 py-2 rounded-lg hover:bg-red-700 transition"
           disabled={loading}
         >
-          {loading ? "Downloading..." : "Download"}
+          {loading ? "Downloading.." : "Download"}
         </button>
       </div>
-
-<a
-  href="https://github.com/karsh0/"
-  target="_blank"
-  rel="noopener noreferrer"
-  className="fixed bottom-5 right-5 bg-black text-white p-3 rounded-full shadow-lg hover:bg-gray-800 transition"
-  aria-label="GitHub Repository"
->
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="currentColor"
-    viewBox="0 0 24 24"
-    className="w-6 h-6"
-  >
-    <path d="M12 0C5.37 0 0 5.373 0 12c0 5.302 3.438 9.8 8.205 11.387.6.11.82-.26.82-.577v-2.23c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.757-1.333-1.757-1.09-.745.083-.73.083-.73 1.205.086 1.84 1.238 1.84 1.238 1.07 1.835 2.807 1.304 3.492.997.107-.775.42-1.304.762-1.604-2.665-.306-5.466-1.334-5.466-5.931 0-1.31.468-2.381 1.235-3.221-.123-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.3 1.23a11.52 11.52 0 013.003-.404c1.02.005 2.047.138 3.003.404 2.29-1.553 3.295-1.23 3.295-1.23.653 1.653.24 2.873.118 3.176.77.84 1.233 1.911 1.233 3.221 0 4.61-2.804 5.622-5.475 5.921.431.372.815 1.103.815 2.222v3.293c0 .319.218.694.825.576C20.565 21.796 24 17.3 24 12c0-6.627-5.373-12-12-12z" />
-  </svg>
-</a>
-
     </div>
   );
 }
